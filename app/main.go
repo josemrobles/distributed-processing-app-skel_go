6f082bf628c80849a501bf64572af1e0f9432ff0
Jose M. Robles <das.haas@gmail.com>
2016-05-02 21:25:56 -0400

    Update main.go

--- app/main.go
+++ app/main.go
@@ -10,8 +10,8 @@ import (
 )
 
 var (
-	port                     = flag.String("port", "8080", "API Port")
-	uri                      = flag.String("uri", "amqp://guest:guest@192.168.99.100:5672/", "AMQP URI")
+	port = flag.String("port", "8080", "API Port")
+	uri = flag.String("uri", "amqp://guest:guest@192.168.99.100:5672/", "AMQP URI")
 	rmqConn *amqp.Connection = nil
 )
 
