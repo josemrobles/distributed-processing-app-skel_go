42f3a32237deb10ef548122fa4a0f01c2c132ffc
josemrobles <joserobles@robi.local>
2016-06-15 19:52:10 -0400

    meh, tired

--- cookbooks/_packaged/dps_app/cookbooks/dps_app/recipes/databag_test.rb
+++
@@ -1,8 +0,0 @@
-search("aws_opsworks_layer").each do |layer|
-	if layer['name'] == 'queue'
-		instances = search("aws_opsworks_instance", "layer_ids:" + "#{layer['layer_id']}"  + ' AND status:online')
-		instances.each do |instance|
-			Chef::Log.warn("********** The instance's ip is '#{instance['private_ip']}' **********")
-		end
-	end
-end
