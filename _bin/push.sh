b6d603d20fab1a96b8a5d4d4b509e564fae6de75
josemrobles <joserobles@robi.broward.edu>
2016-06-29 19:36:36 -0400

    Changes to push.sh

--- _bin/push.sh
+++ _bin/push.sh
@@ -2,15 +2,16 @@
 #!/bin/bash -e
 cookbook_name=$1
 namespace="$2"
+location="$2"
 if [ "$1" == "" ]
 then
 	echo "missing cookbook name as first argument"
 	exit 1
 fi
 if [ "$namespace" != "" ]; then
-	destination="s3://kt-opsworks/${namespace}/$cookbook_name.tar.gz"
+	destination="s3://${location}/${namespace}/$cookbook_name.tar.gz"
 else
-	destination="s3://kt-opsworks/$cookbook_name.tar.gz"
+	destination="s3://${location}/$cookbook_name.tar.gz"
 fi
 echo $destination
 (
